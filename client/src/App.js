f53ba4ef5affa4bcdccd719290a390ade974f8a0
Kornel <srokakornel@gmail.com>
2021-10-15 15:36:29 +0200

    Boilerplate for front

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
+
+const App = () => {
+  return <div>Portfolio</div>;
+}
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
