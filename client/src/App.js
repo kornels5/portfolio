54def1bec62c9c1ad9184657e6a93fce586bcb94
Kornel <srokakornel@gmail.com>
2021-10-20 22:24:24 +0200

    Added tailwind and postCSS config with dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,10 @@ import ReactDOM from 'react-dom';
 import { StrictMode } from 'react';
 
 const App = () => {
-  return <div>Portfolio</div>;
+  return (
+    <header className="w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 to-red-500">
+      Portfolio
+    </header>);
 }
 
 ReactDOM.render(
